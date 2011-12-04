f4697f522b9e8696b215e6bceef64cec2ee55d90
Dirk <dirk@popskorn.de>
2011-12-04 23:05:50 +0100

    Abstrakte Klassen für Mutable und Immutable Trees erstellt und Tester angepasst

--- MutableTree.java
+++ MutableTree.java
@@ -1,19 +1,31 @@
-public abstract class ImmutableTree extends Tree {
-	public abstract ImmutableTree getLeft();
+public abstract class MutableTree extends Tree {
+	public abstract MutableTree getLeft();
 
-	public abstract ImmutableTree getRight();
-	
+	public abstract void setLeft(MutableTree left);
+
+	public abstract MutableTree getRight();
+
+	public abstract void setRight(MutableTree right);
+
+	public abstract void setValue(int value);
+
 	/**
 	 * Abweichend von der urspruenglichen Aufgabenstellung eine Factory-Methode.
 	 */
-	public abstract ImmutableTree newInstance(int value);
+	public abstract MutableTree newInstance(int value);
+
+	/**
+	 * Liefert eine Kopie des Baums. Modifikationen an der Kopie verursachen
+	 * keine Modifikation am urspruenglichen Baum.
+	 */
+	public abstract Tree copy();
 
 	/**
 	 * Fuegt den uebergebenen Wert in den Baum ein, so dass dieser in seiner
 	 * textuellen Repraesentation aufsteigend sortiert ist. Hier ist keine
 	 * Umstrukturierung des Baums erlaubt.
 	 */
-	public abstract ImmutableTree sortedInsert(int insertVal);
+	public abstract void sortedInsert(int newValue);
 
 	/**
 	 * Liefert einen neuen Baum, in welchem das Element aus dem uebergebenen
@@ -22,5 +34,5 @@ public abstract class ImmutableTree extends Tree {
 	 * Element geloescht. Dabei soll die textuelle Repraesentation des
 	 * resultierenden Baums aufsteigend sortiert sein, falls sie es vorher war.
 	 */
-	public abstract ImmutableTree sortedDelete(int deleteVal);
+	public abstract MutableTree sortedDelete(MutableTree deleteFrom, int toDelete);
 }
