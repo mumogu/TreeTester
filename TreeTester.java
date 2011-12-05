bbef2269a5245804bd3927b44d6fbc094c19c2e8
Dirk <dirk@popskorn.de>
2011-12-05 19:47:41 +0100

    Spielregeln hinzugefügt

--- TreeTester.java
+++ TreeTester.java
@@ -2,7 +2,6 @@ import java.util.Random;
 import java.util.Set;
 import java.util.HashSet;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 
 public class TreeTester {
